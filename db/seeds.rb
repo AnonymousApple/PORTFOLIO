e3c06e3b1366c6b4f16d1e8d10e1015c71d13aa8
Timothy Amadi <timothy@Timothys-MacBook-Air.local>
2017-09-09 11:31:12 -0400

    dfcs

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@ Project.destroy_all
 Project.create([{name: 'eCommerce',
                  deploy_url: 'https://anonymousapple-ecommerce.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/eCommerce',
-                 description: 'Simple eCommerce App',
+                 description: 'Simple eCommerce App made with the Stripe API',
                  image: 'ecommerce logo  - Google Search.png'}])
 Project.create([{name: 'martaBusAPI',
                  deploy_url: 'https://marta-bus-api.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/martaBusAPI',
-                 description: 'marta bus',
+                 description: 'marta bus API(Only works for Georgia)',
                  image: 'marta bus  - Google Search.png'}])
 Project.create([{name: 'Twitter',
                  deploy_url: 'https://rubyonrails-twitter.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/twitter-copy',
-                 description: 'Easy twitter app',
+                 description: 'Simple twitter knockout',
                  image: 'twitter  - Google Search.png'}])
 Project.create([{name: 'K4T',
                  deploy_url: 'https://fbc-k4t.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/Bible_study',
-                 description: 'Hi',
+                 description: 'Bible App to help Kids',
                  image: 'bible  - Google Search.png'}])
