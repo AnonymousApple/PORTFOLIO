3bf3a3b4286d92adfac4dc7ae5611b9e66cd8a03
Timothy Amadi <timothy@Timothys-Air.attlocal.net>
2017-09-09 19:59:26 -0400

    gh

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,23 @@
 Project.destroy_all
-Project.create([{name: 'eCommerce',
+Project.create([{name: 'Dete',
                  deploy_url: 'https://anonymousapple-ecommerce.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/eCommerce',
-                 description: 'Dete is a family-owned online restaurant focused on healthy, organic homemade food made from farm-fresh produce. With focus on the health of our customers in mind, we are ensuring that we provide chemical and preservative free meals. Our specialty is bringing international cuisine to your table.',
-                 image: 'ecommerce logo  - Google Search.png'}])
+                 description: 'Dete is an acronym of the names of my awesome family: Daniel, Eugene, Timothy, and Eugenia.<br /><i>Our passion for cooking healthy</i> led to a startup restaurant with focus on serving healthy food prepared from organic fresh farm produce. We serve real food prepared with your health in mind. Our recepies are created from <i>research</i> on the <b>healthiest</b> combination of our ingredients. Farm-fresh spices are a great part of our recepie.'}])
 Project.create([{name: 'martaBusAPI',
                  deploy_url: 'https://marta-bus-api.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/martaBusAPI',
-                 description: 'marta bus API(Only works for Georgia)',
-                 image: 'marta bus  - Google Search.png'}])
+                 description: 'This utilizes the Atlanta bus transit authority web API and <b>Google Maps</b> to display locatios of buses to the user in real time.<br />It also uses the <i>geocoder gem</i> to retreive <i>laitude and longitude</i> coordinates from <em>user input</em>.'}])
 Project.create([{name: 'Twitter',
                  deploy_url: 'https://rubyonrails-twitter.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/twitter-copy',
-                 description: 'Simple twitter knockout',
-                 image: 'twitter  - Google Search.png'}])
+                 description: 'This mimics some of the functionality of Twitter by implementing:
+                 <ul>
+                  <li>User accounts</li>,
+                  <li>followers</li>,
+                  <li>hashtags</li>,
+                 </ul>
+                 and URL shorteners.'}])
 Project.create([{name: 'K4T',
                  deploy_url: 'https://fbc-k4t.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/Bible_study',
-                 description: 'Bible App to help Kids',
-                 image: 'bible  - Google Search.png'}])
+                 description: 'Bible App to help Kids'}])
