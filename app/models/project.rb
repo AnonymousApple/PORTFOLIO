0136b4b76482e67a75db0e181e9768c040bd11a7
Timothy Amadi <timothy@Timothys-MacBook-Air.local>
2017-09-09 10:56:50 -0400

    fodsdljbfdksbgsjfs
    foods
    actual words here

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
   validates :name, presence: true
+  validates :deploy_url, :source_url, presence: true
   mount_uploader :image, ImageUploader
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
+Project.destroy_all!
 Project.create([{name: 'eCommerce',
                  deploy_url: 'https://anonymousapple-ecommerce.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/eCommerce',
                  description: 'Simple eCommerce App'}])
 Project.create([{name: 'martaBusAPI',
-                 deploy_url: '#',
+                 deploy_url: 'https://marta-bus-api.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/martaBusAPI',
                  description: 'marta bus'}])
+Project.create([{name: 'Twitter',
+                 deploy_url: 'https://rubyonrails-twitter.herokuapp.com/',
+                 source_url: 'https://github.com/AnonymousApple/twitter-copy',
+                 description: 'Easy twitter app'}])
+Project.create([{name: 'K4T',
+                 deploy_url: 'https://fbc-k4t.herokuapp.com/',
+                 source_url: 'https://github.com/AnonymousApple/Bible_study',
+                 description: 'Hi'}])
