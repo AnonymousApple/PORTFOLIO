d1acaf799dea4ae3e887836152613716346a9ea9
Timothy Amadi <timothy@Timothys-Air.attlocal.net>
2017-09-09 20:03:38 -0400

    fg

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Project.destroy_all
 Project.create([{name: 'Dete',
                  deploy_url: 'https://anonymousapple-ecommerce.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/eCommerce',
-                 description: 'Dete is an acronym of the names of my awesome family: Daniel, Eugene, Timothy, and Eugenia.<br /><i>Our passion for cooking healthy</i> led to a startup restaurant with focus on serving healthy food prepared from organic fresh farm produce. We serve real food prepared with your health in mind. Our recepies are created from <i>research</i> on the <b>healthiest</b> combination of our ingredients. Farm-fresh spices are a great part of our recepie.'}])
+                 description: 'Dete is an acronym of the names of my awesome family: Daniel, Eugene, Timothy, and Eugenia.<br /><br /><i>Our passion for cooking healthy</i> led to a startup restaurant with focus on serving healthy food prepared from organic fresh farm produce. We serve real food prepared with your health in mind. Our recepies are created from <i>research</i> on the <b>healthiest</b> combination of our ingredients. Farm-fresh spices are a great part of our recepie.'}])
 Project.create([{name: 'martaBusAPI',
                  deploy_url: 'https://marta-bus-api.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/martaBusAPI',
