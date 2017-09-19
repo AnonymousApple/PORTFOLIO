d4c3427e2d20e1e72729a93c23cac0735522dcc1
Timothy Amadi <timothy@Timothys-Air.attlocal.net>
2017-09-19 18:48:51 -0400

    Update the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,6 @@ Project.create([{name: 'Dete',
                  deploy_url: 'https://anonymousapple-ecommerce.herokuapp.com/',
                  source_url: 'https://github.com/AnonymousApple/eCommerce',
                  description: 'Dete is an acronym of the names of my awesome family: Daniel, Eugene, Timothy, and Eugenia.<br /><br /><i>Our passion for cooking healthy</i> led to a startup restaurant with focus on serving healthy food prepared from organic fresh farm produce. We serve real food prepared with your health in mind. Our recepies are created from <i>research</i> on the <b>healthiest</b> combination of our ingredients. Farm-fresh spices are a great part of our recepie.'}])
-Project.create([{name: 'martaBusAPI',
-                 deploy_url: 'https://marta-bus-api.herokuapp.com/',
-                 source_url: 'https://github.com/AnonymousApple/martaBusAPI',
                  description: 'This utilizes the Atlanta bus transit authority web API and <b>Google Maps</b> to display locatios of buses to the user in real time.<br />It also uses the <i>geocoder gem</i> to retreive <i>laitude and longitude</i> coordinates from <em>user input</em>.'}])
 Project.create([{name: 'Twitter',
                  deploy_url: 'https://rubyonrails-twitter.herokuapp.com/',
@@ -17,7 +14,7 @@ Project.create([{name: 'Twitter',
                   <li>hashtags,</li>
                  </ul>
                  and URL shorteners.'}])
-# Project.create([{name: 'K4T',
-#                  deploy_url: 'https://fbc-k4t.herokuapp.com/',
-#                  source_url: 'https://github.com/AnonymousApple/Bible_study',
-#                  description: 'Bible App to help Kids'}])
+Project.create([{name: 'K4T',
+                 deploy_url: 'https://fbc-k4t.herokuapp.com/',
+                 source_url: 'https://github.com/AnonymousApple/Bible_study',
+                 description: 'Bible App to help Kids'}])
